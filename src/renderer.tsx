9a1110d72cd5cd5931e507c38ea40394d4b50817
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-12-13 22:04:45 +0530

    Added react support

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -26,6 +26,17 @@
  * ```
  */
 
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/app';
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+// Modern React 18 rendering
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
