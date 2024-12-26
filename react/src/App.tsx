98fe268df32efaaa9e89643d9758bc4dc71e1d64
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-12-26 06:25:33 +0530

    Just creating a commit the work to change computers

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,9 +2,9 @@ import {FunctionComponent} from 'react';
 
 import "./global.scss";
 
-import {MainLayout} from '@components/layout/main-layout';
-import {Footer} from "@components/layout/footer";
-import {Navbar} from "@components/layout/navbar";
+import {MainLayout} from '@components/layout/main-layout/MainLayout';
+import {Footer} from "@components/layout/footer/Footer";
+import {Navbar} from "@components/layout/navbar/Navbar";
 
 const App: FunctionComponent = () => {
     return (
--- react/src/components/layout/navbar/Navbar.tsx
+++ react/src/components/layout/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import styles from './Navbar.module.scss';
+import { EnvironmentSwitcher } from './components/environment-switcher/EnvironmentSwitcher';
 
 import {
     ReorderFourOutline,
@@ -11,32 +12,59 @@ import {
 } from "@icons";
 
 export const Navbar: FunctionComponent = () => {
+    const [isEnvironmentSwitcherOpen, setIsEnvironmentSwitcherOpen] = useState(false);
+
+    const handleEnvironmentClick = () => {
+        setIsEnvironmentSwitcherOpen(true);
+    };
+
+    const handleEnvironmentClose = () => {
+        setIsEnvironmentSwitcherOpen(false);
+    };
+
     return (
-        <div className={styles.navbar}>
-            <div className={styles.menuWrapper}>
-                <img alt="Menu" src={ReorderFourOutline} />
-            </div>
-
-            <div className={styles.environmentSection}>
-                <div className={styles.environmentSelect}>
-                    <span>Environment</span>
-                </div>
-                <img alt="Options" src={DotsVertical} />
-            </div>
-
-            <div className={styles.mainSection}>
-                <div className={styles.searchBar}>
-                    <img alt="Search" src={Search} />
-                    <span>Search</span>
+        <>
+            <div className={styles.navbar}>
+                <div className={styles.menuWrapper}>
+                    <img alt="Menu" src={ReorderFourOutline} />
                 </div>
 
-                <div className={styles.actionButtons}>
-                    <img alt="Inbox" src={Inbox} />
-                    <img alt="Notifications" src={NotificationsOutline} />
-                    <img alt="Settings" src={Settings} />
-                    <div className={styles.userAvatar} />
+                <div className={styles.environmentSection}>
+                    <div
+                        className={styles.environmentSelect}
+                        onClick={handleEnvironmentClick}
+                        role="button"
+                        tabIndex={0}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter' || e.key === ' ') {
+                                handleEnvironmentClick();
+                            }
+                        }}
+                    >
+                        <span>Environment</span>
+                    </div>
+                    <img alt="Options" src={DotsVertical} />
+                </div>
+
+                <div className={styles.mainSection}>
+                    <div className={styles.searchBar}>
+                        <img alt="Search" src={Search} />
+                        <span>Search</span>
+                    </div>
+
+                    <div className={styles.actionButtons}>
+                        <img alt="Inbox" src={Inbox} />
+                        <img alt="Notifications" src={NotificationsOutline} />
+                        <img alt="Settings" src={Settings} />
+                        <div className={styles.userAvatar} />
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <EnvironmentSwitcher
+                isOpen={isEnvironmentSwitcherOpen}
+                onClose={handleEnvironmentClose}
+            />
+        </>
     );
 };
